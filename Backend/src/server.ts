1d9644106b130f6bb737037b2218667c6016a6f5
Andresflorezdev <florezandresfelipe62@gmail.com>
2026-04-14 14:43:48 -0500

    Add CORS configuration for frontend integration

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from "cors";
 import 'dotenv/config';
 import router from './router';
+import { corsConfig } from './config/cors';
 
 const server = express();
+
+// Cors
+server.use(cors(corsConfig))
 
 server.use(express.json())
 
