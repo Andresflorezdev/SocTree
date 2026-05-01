f8b94ec23c34f8e21a83644093374edabcaaac11
Andresflorezdev <florezandresfelipe62@gmail.com>
2026-04-30 21:13:15 -0500

    Add authenticated dashboard layout and navigation

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,18 +2,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginView from './view/LoginView';
 import { RegisterView } from './view/RegisterView';
 import AuthLayout from './layouts/AuthLayout';
+import AppLayout from './layouts/AppLayout';
+import LinkTreeView from './view/LinkTreeView';
+import ProfileView from './view/ProfileView';
 
 export default function Router() {
-    
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<AuthLayout/>}>
-                    <Route path='/auth/login' element={<LoginView/>} />
-                    <Route path='/auth/register' element={<RegisterView/>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-
-};
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+        
+        <Route path='/admin' element={<AppLayout/>}>
+          <Route index={true} element={<LinkTreeView/>} />
+          <Route path='profile' element={<ProfileView/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
