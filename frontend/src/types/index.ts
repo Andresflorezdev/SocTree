6ecbb38f5959b39c1c415ea1d81b19d593fd8077
Andresflorezdev <florezandresfelipe62@gmail.com>
2026-04-30 21:12:51 -0500

    Refine shared types and authentication UI components

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,6 +3,8 @@ export type User = {
     handle: string,
     name: string,
     email: string,
+    _id: string,
+    description: string,
 };
 
 export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
@@ -10,3 +12,8 @@ export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
     password_confirmation: string
 };
 
+export type LoginForm = Pick<User, 'email'> & {
+    password: string,
+};
+
+export type ProfileForm = Pick<User, 'handle' | 'description'>
